9acdac5cf39a835587f492140b6bb22794a855c2
Shubham Singh <shubhamsinghvr2n@gmail.com>
2025-06-18 11:11:35 +0000

    database

--- frontend/database.py
+++ frontend/database.py
@@ -7,5 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL is None:
+    raise ValueError("❌ DATABASE_URL not found in environment")
+    
 engine = create_engine(DATABASE_URL, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
